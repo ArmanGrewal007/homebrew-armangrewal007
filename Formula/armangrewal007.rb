529d0595f848ad525a7f07bd1a9f0d150c74c319
ArmanGrewal007 <armansinghgrewal@gmail.com>
2024-09-24 08:56:17 +0530

    Added python dependency

--- Formula/armangrewal007.rb
+++ Formula/armangrewal007.rb
@@ -4,7 +4,7 @@ class Armangrewal007 < Formula
   url "https://files.pythonhosted.org/packages/source/a/armangrewal007/armangrewal007-0.1.2.tar.gz" 
   sha256 "38b561affa07997dea3dc34c9dcd9cc3f5587204ed8520d95bb05ab34ce37971" 
 
-  # depends_on "python@3.9"  # Specify the Python version 
+  depends_on "python@3.10"  # Specify the Python version 
 
   def install
     system "pip", "install", "--prefix=#{prefix}", "--upgrade", "armangrewal007"
