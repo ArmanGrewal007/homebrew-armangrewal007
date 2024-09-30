8accd6a26e207970c772e0f6385e7ffd835131c5
ArmanGrewal007 <armansinghgrewal@gmail.com>
2024-09-30 18:09:27 +0530

    v0.1.3

--- Formula/armangrewal007.rb
+++ Formula/armangrewal007.rb
@@ -1,8 +1,8 @@
 class Armangrewal007 < Formula
   desc "CLI to display Arman Singh Grewal's resume"
   homepage "https://github.com/yourusername/armangrewal007"
-  url "https://files.pythonhosted.org/packages/source/a/armangrewal007/armangrewal007-0.1.2.tar.gz" 
-  sha256 "38b561affa07997dea3dc34c9dcd9cc3f5587204ed8520d95bb05ab34ce37971" 
+  url "https://files.pythonhosted.org/packages/source/a/armangrewal007/armangrewal007-0.1.3.tar.gz" 
+  sha256 "7e5ffcce261e9997a164c9c245832ad16e2aec9a7291040d0c2b3dc6ee16c992" 
 
   depends_on "python@3.10"  # Specify the Python version 
 
